053508582326071a8a36e76ce5e632d025454f7b
yanwu-728 <74961465+yanwu-728@users.noreply.github.com>
2022-05-02 22:18:04 -0400

    Added an example code of using ArduCam by clicking button

---
+++ camera_example/camera_example.ino
@@ -0,0 +1,232 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClient.h>
+#include <Wire.h>
+//#include <ESP32WebServer.h>
+#include <ArduCAM.h>
+#include <SPI.h>
+#include <string.h>
+//#include <ArduinoJson.h>
+#include "memorysaver.h"
+
+#if !(defined ESP32 )
+#error Please select the ArduCAM ESP32 UNO board in the Tools/Board
+#endif
+//This demo can only work on OV2640_MINI_2MP or ARDUCAM_SHIELD_V2 platform.
+#if !(defined (OV2640_MINI_2MP)||defined (OV5640_MINI_5MP_PLUS) || defined (OV5642_MINI_5MP_PLUS) \
+    || defined (OV5642_MINI_5MP) || defined (OV5642_MINI_5MP_BIT_ROTATION_FIXED) \
+    ||(defined (ARDUCAM_SHIELD_V2) && (defined (OV2640_CAM) || defined (OV5640_CAM) || defined (OV5642_CAM))))
+#error Please select the hardware platform and camera module in the ../libraries/ArduCAM/memorysaver.h file
+#endif
+
+// set GPIO34 as the slave select :
+const int CS = 34;
+const int CAM_POWER_ON = 10;
+const int KEY = 45;
+WiFiClientSecure client; //global WiFiClient Secure object
+
+ArduCAM myCAM(OV2640, CS);
+
+static const size_t bufferSize = 8000;
+static uint8_t buffer[bufferSize] = {0xFF};
+uint8_t temp = 0, temp_last = 0;
+int i = 0;
+bool is_header = false;
+
+int ind=0;
+int temp_1 = buffer[ind];
+int temp_last_1;
+char holder[bufferSize];
+char image_data[8000];
+char body[6000];
+
+const uint16_t RESPONSE_TIMEOUT = 6000;
+const uint16_t IN_BUFFER_SIZE = 8000; //size of buffer to hold HTTP request
+const uint16_t OUT_BUFFER_SIZE = 8000; //size of buffer to hold HTTP response
+//const uint16_t JSON_BODY_SIZE = 3000;
+char request_buffer[IN_BUFFER_SIZE];
+char response_buffer[OUT_BUFFER_SIZE]; //char array buffer to hold HTTP request
+char json_body[IN_BUFFER_SIZE];
+
+char network[] = "MIT";  //SSID for 6.08 Lab
+char password[] = ""; //Password for 6.08 Lab
+
+//ESP32WebServer server(80);
+
+void start_capture() {
+  myCAM.clear_fifo_flag();
+  myCAM.start_capture();
+}
+
+void camCapture(ArduCAM myCAM) {
+  uint32_t len  = myCAM.read_fifo_length();
+  if (len >= MAX_FIFO_SIZE) //8M
+  {
+    Serial.println(F("Over size."));
+  }
+  if (len == 0 ) //0 kb
+  {
+    Serial.println(F("Size is 0."));
+  }
+  myCAM.CS_LOW();
+  myCAM.set_fifo_burst();
+  
+  i = 0;
+  while ( len-- )
+  {
+    temp_last = temp;
+    temp =  SPI.transfer(0x00);
+    //Read JPEG data from FIFO
+    if ( (temp == 0xD9) && (temp_last == 0xFF) ) //If find the end ,break while,
+    {
+      buffer[i++] = temp;  //save the last  0XD9
+      //Write the remain bytes in the buffer
+      is_header = false;
+      myCAM.CS_HIGH();
+      break;
+    }
+    if (is_header == true)
+    {
+      //Write image data to buffer if not full
+      if (i < bufferSize){
+        buffer[i++] = temp;
+      }
+      else
+      {
+        //Write bufferSize bytes image data to file
+        i = 0;
+        buffer[i++] = temp;
+      }
+    }
+    else if ((temp == 0xD8) & (temp_last == 0xFF))
+    {
+      is_header = true;
+      buffer[i++] = temp_last;
+      buffer[i++] = temp;
+    }
+  }
+  memcpy(holder, buffer, sizeof(buffer));
+  memset(image_data, 0, sizeof(image_data));
+  memset(body, 0, sizeof(body));
+
+  sprintf(body,"{\"location\":\'Test\', \"user_id\":0, \"image_encoding\":\'%s\'}", image_data);
+  memset(request_buffer, 0, IN_BUFFER_SIZE);
+  memset(response_buffer, 0, OUT_BUFFER_SIZE);
+  request_buffer[0] = '\0'; //set 0th byte to null
+  int offset = 0; 
+  offset += sprintf(request_buffer + offset, "POST /sandbox/sc/team42/608_team42_final/image_request.py HTTP/1.1\r\n");
+  offset += sprintf(request_buffer + offset, "Host: 608dev-2.net\r\n");
+  offset += sprintf(request_buffer + offset, "Content-Type: application/json\r\n");
+  offset += sprintf(request_buffer + offset, "Content-Length: %d\r\n\r\n", strlen(body));
+  offset += sprintf(request_buffer + offset, body);
+  do_http_request("608dev-2.net", request_buffer, response_buffer, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, false);
+  Serial.println("-----------");
+  Serial.println(response_buffer);
+  Serial.println("-----------");
+}
+
+void serverCapture() {
+  delay(1000);
+  start_capture();
+  Serial.println(F("CAM Capturing"));
+
+  int total_time = 0;
+
+  total_time = millis();
+  while (!myCAM.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK));
+  total_time = millis() - total_time;
+  Serial.print(F("capture total_time used (in miliseconds):"));
+  Serial.println(total_time, DEC);
+
+  total_time = 0;
+
+  Serial.println(F("CAM Capture Done."));
+  total_time = millis();
+  camCapture(myCAM);
+  total_time = millis() - total_time;
+  Serial.print(F("send total_time used (in miliseconds):"));
+  Serial.println(total_time, DEC);
+  Serial.println(F("CAM send Done."));
+}
+
+void setup() {
+  // Wifi connecting code
+  Serial.begin(115200); //for debugging if needed.
+  WiFi.begin(network, password); //attempt to connect to wifi
+  uint8_t count = 0; //count used for Wifi check times
+  Serial.print("Attempting to connect to ");
+  Serial.println(network);
+  while (WiFi.status() != WL_CONNECTED && count < 12) {
+    delay(500);
+    Serial.print(".");
+    count++;
+  }
+  delay(2000);
+  if (WiFi.isConnected()) { //if we connected then print our IP, Mac, and SSID we're on
+    Serial.println("CONNECTED!");
+    Serial.printf("%d:%d:%d:%d (%s) (%s)\n", WiFi.localIP()[3], WiFi.localIP()[2],
+                  WiFi.localIP()[1], WiFi.localIP()[0],
+                  WiFi.macAddress().c_str() , WiFi.SSID().c_str());    delay(500);
+  } else { //if we failed to connect just Try again.
+    Serial.println("Failed to Connect :/  Going to restart");
+    Serial.println(WiFi.status());
+    ESP.restart(); // restart the ESP (proper way)
+  }
+  
+  uint8_t vid, pid;
+  uint8_t temp;
+  //set the CS as an output:
+  pinMode(CS, OUTPUT);
+  pinMode(CAM_POWER_ON , OUTPUT);
+  pinMode(KEY, INPUT_PULLUP);
+  digitalWrite(CAM_POWER_ON, HIGH);
+#if defined(__SAM3X8E__)
+  Wire1.begin();
+#else
+  Wire.begin();
+#endif
+//  Serial.begin(115200);
+  Serial.println(F("ArduCAM Start!"));
+
+  // initialize SPI:
+  SPI.begin();
+  SPI.setFrequency(4000000); //4MHz
+
+  //Check if the ArduCAM SPI bus is OK
+  myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
+  temp = myCAM.read_reg(ARDUCHIP_TEST1);
+  if (temp != 0x55) {
+    Serial.println(F("SPI1 interface Error!"));
+    while (1);
+  }
+
+  //Check if the ArduCAM SPI bus is OK
+  myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
+  temp = myCAM.read_reg(ARDUCHIP_TEST1);
+  if (temp != 0x55) {
+    Serial.println(F("SPI1 interface Error!"));
+    while (1);
+  }
+  
+  //Check if the camera module type is OV2640
+  myCAM.wrSensorReg8_8(0xff, 0x01);
+  myCAM.rdSensorReg8_8(OV2640_CHIPID_HIGH, &vid);
+  myCAM.rdSensorReg8_8(OV2640_CHIPID_LOW, &pid);
+  if ((vid != 0x26 ) && (( pid != 0x41 ) || ( pid != 0x42 )))
+    Serial.println(F("Can't find OV2640 module!"));
+  else
+    Serial.println(F("OV2640 detected."));
+
+  //Change to JPEG capture mode and initialize the OV2640 module
+  myCAM.set_format(JPEG);
+  myCAM.InitCAM();
+  myCAM.OV2640_set_JPEG_size(OV2640_160x120);
+  myCAM.clear_fifo_flag();
+}
+
+void loop() {
+   uint8_t key = digitalRead(KEY);
+   if(!key){
+    serverCapture();
+   }
+}
